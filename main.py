15e411cd169529194f416953133fb0493d5bcada
Am1ne-bou <boussennaa.a7@gmail.com>
2025-06-02 16:26:20 +0200

    home page

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Ondes Sismiques", page_icon=":earth_americas:", layout="wide")
+
+st.title("Ondes Sismiques : Ateliers Interactifs")
+
+st.sidebar.success("Choisissez un atelier pour commencer :")
+

b5b967e84be28202346ab67a95fadd35f941f5aa
Am1ne-bou <boussennaa.a7@gmail.com>
2025-06-15 23:27:38 +0200

    Update 06 _ epicentre.py

--- pages/06 _ epicentre.py
+++ pages/06 _ epicentre.py
@@ -295,7 +295,7 @@ if signal_choice == "Nord-Sud":
         step=0.1,
         key='slider_ts'
     )
-
+    fig_puissance_x.add_vline(x=t_s_x, line_dash="dash", line_color="cyan", annotation_text="Onde S")
     if st.button("Verification des temps d'arrivée de l'onde S"):
         signal_s = bandpass_filter(signal_x, 0.5, 2, fs)  # Onde S
 
@@ -346,7 +346,7 @@ if signal_choice == "Est-Ouest":
         step=0.1,
         key='slider_ts'
     )
-
+    fig_puissance_y.add_vline(x=t_s_y, line_dash="dash", line_color="orange", annotation_text="Onde S")
     if st.button("Verification des temps d'arrivée de l'onde S"):
         signal_s = bandpass_filter(signal_y, 0.5, 2, fs)  # Onde S
 
@@ -395,7 +395,7 @@ if signal_choice == "Vertical":
         step=0.1,
         key='slider_ts'
     )
-
+    fig_puissance_z.add_vline(x=t_p_z, line_dash="dash", line_color="yellow", annotation_text="Onde P")
     if st.button("Verification des temps d'arrivée de l'onde P"):
         signal_p = bandpass_filter(signal_z, 8, 12, fs)  # Onde P
 
